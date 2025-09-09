23df763dc3d88ea60e4959b2d2eff023ca457e98
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-09-09 15:11:18 -0600

    db: create core querys to be used

--- db/test_querys.sql
+++ db/test_querys.sql
@@ -7,3 +7,72 @@ SELECT * FROM resource_types;
 SELECT * FROM topics;
 
 SELECT * FROM resources;
+
+-- Random topic without filters
+SELECT 
+    t.id AS topic_id,
+    t.name AS topic_name,
+    t.description AS topic_description,
+    c.name AS category_name,
+    c.description AS category_description,
+    d.name AS difficulty_name,
+    d.description AS difficulty_description
+FROM topics t
+JOIN categories c ON c.id = t.category_id
+JOIN difficulties d ON d.id = t.difficulty_id
+WHERE t.is_active = TRUE
+ORDER BY RAND()
+LIMIT 1;
+
+-- Random topic with category filter
+SELECT 
+    t.id AS topic_id,
+    t.name AS topic_name,
+    t.description AS topic_description,
+    c.name AS category_name,
+    c.description AS category_description,
+    d.name AS difficulty_name,
+    d.description AS difficulty_description
+FROM topics t
+JOIN categories c ON c.id = t.category_id
+JOIN difficulties d ON d.id = t.difficulty_id
+WHERE t.is_active = TRUE
+AND c.name = "Technology"
+ORDER BY RAND()
+LIMIT 1;
+
+-- Random topic with difficulty filter
+SELECT 
+    t.id AS topic_id,
+    t.name AS topic_name,
+    t.description AS topic_description,
+    c.name AS category_name,
+    c.description AS category_description,
+    d.name AS difficulty_name,
+    d.description AS difficulty_description
+FROM topics t
+JOIN categories c ON c.id = t.category_id
+JOIN difficulties d ON d.id = t.difficulty_id
+WHERE t.is_active = TRUE
+AND d.name = "Beginner"
+ORDER BY RAND()
+LIMIT 1;
+
+-- Random topic with category and difficulty filters
+SELECT 
+    t.id AS topic_id,
+    t.name AS topic_name,
+    t.description AS topic_description,
+    c.name AS category_name,
+    c.description AS category_description,
+    d.name AS difficulty_name,
+    d.description AS difficulty_description
+FROM topics t
+JOIN categories c ON c.id = t.category_id
+JOIN difficulties d ON d.id = t.difficulty_id
+WHERE t.is_active = TRUE
+AND c.name = "Technology"
+AND d.name = "Beginner"
+ORDER BY RAND()
+LIMIT 1;
+
