ecc4fe8ecf1905664e15cf356a413964bb9b1370
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-09-09 23:55:24 -0600

    db: update files

--- db/script.sh
+++ db/script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Build the image (only once)
 docker build . -t leojimenezg/rtapi-db:v1.0.0
+# Create and run the container
 docker run --name rtapi-db -d -p 3306:3306 --env-file ./db.env leojimenezg/rtapi-db:v1.0.0
