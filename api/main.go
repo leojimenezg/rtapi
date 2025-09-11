237a95ef8571d12ec4e43288b4a468af2f7dd053
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-09-10 19:50:33 -0600

    api: simple connection to database

--- api/main.go
+++ api/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+)
 
+// Temporary local credentials
+const (
+	DATABASE = ""
+	USERNAME = ""
+	PASSWORD = ""
+	ADDRESS = ""
+)
+
+func main() {
+	connectionConfig := mysql.NewConfig()
+	connectionConfig.User = USERNAME
+	connectionConfig.Passwd = PASSWORD
+	connectionConfig.Net = "tcp"
+	connectionConfig.Addr = ADDRESS
+	connectionConfig.DBName = DATABASE
+	db, err := sql.Open("mysql", connectionConfig.FormatDSN())
+	if err != nil { panic(err) }
+	if ping := db.Ping(); ping != nil { panic(ping) }
+	fmt.Printf("Connected to %s database!\n", DATABASE)
+	db.Close()
 }
