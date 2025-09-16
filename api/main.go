f96986b955b933469754f8ef555073d5603e7056
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-09-16 00:05:08 -0600

    api: create base endpoints

--- api/main.go
+++ api/main.go
@@ -10,11 +10,37 @@ import (
 func main() {
 	db, dbErr := database.ConnectToMySQLDB()
 	if dbErr != nil { panic(dbErr) }
-	fmt.Println("Connected to Database!")
 	defer db.Close()
+	fmt.Println("Connected to Database!")
+
 	handler := handlers.New(db)
 	router := gin.Default()
+
+	// Categories endpoints
+	router.GET("/categories", handler.GetAllCategories)
+	router.GET("/categories/:id", handler.GetCategoryById)
+	router.GET("/categories/name/:name", handler.GetCategoryByName)
+
+	// Difficulties endpoints  
+	router.GET("/difficulties", handler.GetAllDifficulties)
+	router.GET("/difficulties/:id", handler.GetDifficultyById)
+	router.GET("/difficulties/name/:name", handler.GetDifficultyByName)
+
+	// Resource_types endpoints
+	router.GET("/resource_types", handler.GetAllResourceTypes)
+	router.GET("/resource_types/:id", handler.GetResourceTypeById)
+	router.GET("/resource_types/name/:name", handler.GetResourceTypeByName)
+
+	// Topics endpoints
 	router.GET("/topics", handler.GetAllTopics)
+	router.GET("/topics/:id", handler.GetTopicById)
+	router.GET("/topics/name/:name", handler.GetTopicByName)
 	router.GET("/topics/details", handler.GetAllTopicsWithDetails)
+	router.GET("/topics/details/:id", handler.GetTopicWithDetailsById)
+	router.GET("/topics/details/name/:name", handler.GetTopicWithDetailsByName)
+
+	// Random topic using query params
+	router.GET("/topics/details/random", handler.GetRandomTopicWithDetails)
+
 	router.Run()
 }
